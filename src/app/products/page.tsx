dfa7b857958cd283c2944f459f7c934f94f90cac
Melqui Morais <melquimoraiss@gmail.com>
2025-02-19 14:26:38 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductPage = () => {
+  return (
+    <div className="p-5 background bg-red-800 shadow-md">
+      <h1 className="text-white">products page</h1>;<Button>imbu</Button>
+      <Input placeholder="bora cagar" />
+    </div>
+  );
+};
+
+export default ProductPage;
