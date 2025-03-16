3f936b1a86c772ee6e19d73dfdfcdf02ecade039
Melqui Morais <melquimoraiss@gmail.com>
2025-03-16 16:32:05 -0300

    fix: order-list status was not working

--- src/app/[slug]/orders/components/order-list.tsx
+++ src/app/[slug]/orders/components/order-list.tsx
@@ -1,10 +1,13 @@
+"use client";
+
 import { OrderStatus, Prisma } from "@prisma/client";
-import { Separator } from "@radix-ui/react-separator";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { formatCurrency } from "@/helpers/format-currency";
 
 interface OrderListProps {
@@ -35,9 +38,16 @@ const getStatusLabel = (status: OrderStatus) => {
 };
 
 const OrderList = ({ orders }: OrderListProps) => {
+  const router = useRouter();
+  const handleBackClick = () => router.back();
   return (
     <div className="space-y-6 p-6">
-      <Button size="icon" variant="secondary" className="rounded-full">
+      <Button
+        size="icon"
+        variant="secondary"
+        className="rounded-full"
+        onClick={handleBackClick}
+      >
         <ChevronLeftIcon />
       </Button>
       <div className="flex items-center gap-3">
@@ -48,11 +58,7 @@ const OrderList = ({ orders }: OrderListProps) => {
         <Card key={order.id}>
           <CardContent className="space-y-4 p-5">
             <div
-              className={`w-fit rounded-full px-2 py-1 text-xs text-white font-semibold${
-                order.status === OrderStatus.FINISHED
-                  ? "bg-green-500 text-white"
-                  : "bg-gray-200 text-gray-500"
-              }`}
+              className={`w-fit rounded-full px-2 py-1 text-xs font-semibold text-white ${([OrderStatus.FINISHED] as OrderStatus[]).includes(order.status) ? "bg-green-500 text-white" : "bg-gray-200 text-gray-500"} `}
             >
               {getStatusLabel(order.status)}
             </div>
@@ -68,14 +74,16 @@ const OrderList = ({ orders }: OrderListProps) => {
               <p className="text-sm font-semibold">{order.restaurant.name}</p>
             </div>
             <Separator />
-            {order.orderProducts.map((orderProduct) => (
-              <div key={orderProduct.id} className="flex items-center gap-2">
-                <div className="flex h-5 w-5 items-center justify-center rounded-full bg-gray-400 font-semibold text-gray-200">
-                  {orderProduct.quantity}
+            <div className="space-y-2">
+              {order.orderProducts.map((orderProduct) => (
+                <div key={orderProduct.id} className="flex items-center gap-2">
+                  <div className="flex h-5 w-5 items-center justify-center rounded-full bg-gray-400 text-xs font-semibold text-white">
+                    {orderProduct.quantity}
+                  </div>
+                  <p className="text-sm">{orderProduct.product.name}</p>
                 </div>
-                <p className="text-sm">{orderProduct.product.name}</p>
-              </div>
-            ))}
+              ))}
+            </div>
             <Separator />
             <p className="text-sm font-medium">{formatCurrency(order.total)}</p>
           </CardContent>
